88fc19b2b2ba6bcec36fa339e0f32852bd254864
hnguyen999 <hnguyen0@chicagobooth.edu>
2021-04-28 19:30:17 +0000

    Added-Bootstraps-CDN

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -20,12 +20,11 @@
 #  fk_rails_...  (sender_id => users.id)
 #
 class FollowRequest < ApplicationRecord
-  belongs_to :recipient
-  belongs_to :sender
+  belongs_to :recipient, class_name: "User"
+  belongs_to :sender, class_name: "User"
 
-  enum status: { pending: "pending", rejected: "rejected", accepted: "accepted" }
+  validates :recipient_id, uniqueness: { scope: :sender_id, message: "already followed" }
 
-  scope :accepted, -> { where(status: "accepted")}
-  scope :not_accepted, -> { where.not(status: "accepted")}
+  enum status: { pending: "pending", rejected: "rejected", accepted: "accepted" } 
 
 end
